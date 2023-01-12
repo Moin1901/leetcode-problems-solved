6695ea04b11f2c8682fcc952f21e635b6cef9f5f
Moin1901 <90569939+Moin1901@users.noreply.github.com>
2023-01-12 11:23:59 +0530

    Time: 9 ms (41.02%), Space: 13.6 MB (76.40%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return build(preorder,i,INT_MAX);
+        
+    }
+    TreeNode *build(vector<int>&A,int &i,int bound){
+        if(i == A.size() || A[i] > bound)return NULL;
+        TreeNode *root=new TreeNode(A[i++]);
+        root->left=build(A,i,root->val);
+        root->right=build(A,i,bound);
+        return root;
+        
+        
+    }
+};
+
+/*class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& A) {
+        int i = 0;
+        return build(A, i, INT_MAX);
+    }
+
+    TreeNode* build(vector<int>& A, int& i, int bound) {
+        if (i == A.size() || A[i] > bound) return NULL;
+        TreeNode* root = new TreeNode(A[i++]);
+        root->left = build(A, i, root->val);
+        root->right = build(A, i, bound);
+        return root;
+    }
+};*/
