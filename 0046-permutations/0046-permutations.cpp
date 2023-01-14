8144da92d0dd0f0d66d424ba576abc8f4a620b70
Moin1901 <90569939+Moin1901@users.noreply.github.com>
2023-01-14 12:19:50 +0530

    Time: 3 ms (81.23%), Space: 7.7 MB (79.72%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,13 +1,5 @@
 class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        perm(0,nums,ans);
-        return ans;
-        
-        
-        
-    }
+private:
     void perm(int index,vector<int>&arr,vector<vector<int>>&ans){
         if(index==arr.size()){
             ans.push_back(arr);
@@ -19,5 +11,14 @@ public:
         swap(arr[index],arr[i]);
             
         }
+    }
+    public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        perm(0,nums,ans);
+        return ans;
+        
+        
+        
     }
 };
