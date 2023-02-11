34daac4483e444feec8d1b353b443dc294248407
Moin1901 <90569939+Moin1901@users.noreply.github.com>
2023-02-11 17:43:19 +0530

    Added solution - LeetHub

---
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        queue<pair<int,int>>q;
+        vector<int>dist(100000,INT_MAX);
+        long long mod=100000;
+        dist[start]=0;
+        q.push({0,start});
+        while(!q.empty()){
+            int step=q.front().first;
+            int node=q.front().second;
+            q.pop();
+            for(int i=0;i<arr.size();i++){
+                int n=node*arr[i]%mod;
+
+                if(step+1<dist[n]){
+                    dist[n]=step+1;
+                    if(n==end)return step+1;
+                    q.push({step+1,n});
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        int start, end;
+        cin >> start >> end;
+        Solution obj;
+        cout << obj.minimumMultiplications(arr, start, end) << endl;
+    }
+}
+
+// } Driver Code Ends
