486ba182a00efa38e5ae032cefa29f183ee5f779
Moin1901 <90569939+Moin1901@users.noreply.github.com>
2023-03-10 20:27:19 +0530

    Time: 2 ms (64.87%), Space: 7.9 MB (61.25%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum=0;
+        vector<int>arr(110,0);
+        for(int i=0;i<nums.size();i++){
+            arr[nums[i]]++;
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==1){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+};
